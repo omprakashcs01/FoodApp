8ba646307e625926ac9115044a65f31230ece2d3
Om Prakash <omprakashcs01@gmail.com>
2024-03-14 12:01:24 +0530

    Basic app

---
+++ FoodApp/App.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import SearchScreen from './src/screens/SearchScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='SearchScreen'>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name = 'SearchScreen' component={SearchScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
